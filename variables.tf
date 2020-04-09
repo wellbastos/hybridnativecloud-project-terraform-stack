8d32e978afb6e65c98cf100221c2e3ac4d09ed37
wellington bastos <wellingtonbastos@hotmail.com>
2020-04-09 02:13:24 +0000

    fix pem

--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "KEY_NAME" {
   default = "fiap-lab"
 }
 variable "PATH_TO_KEY" {
-  default = "/app/.ssh/fiap-lab.pem"
+  default = "/app/.ssh/fiap-lab1.pem"
 }
 variable "INSTANCE_USERNAME" {
   default = "ubuntu"
