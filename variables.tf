df755fc8ffb9981cebcea0ae38cccf0e7f7bfe15
wellington Bastos <wellingtonbastos@hotmail.com>
2020-04-21 20:02:00 +0000

    role with workspace

--- variables.tf
+++ variables.tf
@@ -16,7 +16,8 @@ variable "KEY_NAME" {
   default = "fiap-lab"
 }
 variable "PATH_TO_KEY" {
-  default = "/app/.ssh/fiap-lab.pem"
+  default = ".ssh/fiap-lab.pem"
+ # default = "/home/ubuntu/.ssh/fiap-lab.pem"
 }
 variable "INSTANCE_USERNAME" {
   default = "ubuntu"
